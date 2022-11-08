34dd10be06a604b3bc8f56e60cc97fc9d04dee63
liut <liutao@liut.cc>
2022-11-08 16:48:05 +0800

    add multiple and shorted for enums

--- scripts/codegen/type_enum.go
+++ scripts/codegen/type_enum.go
@@ -7,6 +7,10 @@ import (
 	"github.com/dave/jennifer/jen"
 )
 
+const (
+	shortLen = 3
+)
+
 type Enum struct {
 	Comment string    `yaml:"comment"`
 	Name    string    `yaml:"name"`
@@ -17,11 +21,21 @@ type Enum struct {
 	Decodable       bool `yaml:"decodable,omitempty"`
 	Stringer        bool `yaml:"stringer,omitempty"`
 	TextUnmarshaler bool `yaml:"textUnmarshaler,omitempty"`
+	Multiple        bool `yaml:"multiple,omitempty"`
+	Shorted         bool `yaml:"shorted,omitempty"`
 }
 
 type EnumVal struct {
 	Label  string `yaml:"label,omitempty"`
 	Suffix string `yaml:"suffix"`
+}
+
+func (ev EnumVal) getLabel(shorted bool) string {
+	s := LcFirst(ev.Suffix)
+	if shorted && len(s) > shortLen {
+		return s[:shortLen]
+	}
+	return s
 }
 
 func (e *Enum) Code() jen.Code {
@@ -30,12 +44,24 @@ func (e *Enum) Code() jen.Code {
 
 	if len(e.Values) > 0 {
 		st.Const().DefsFunc(func(g *jen.Group) {
+			op := "+"
+			if e.Multiple {
+				if e.Start < 1 {
+					e.Start = 1
+				}
+				op = "<<"
+			}
 			for i, ev := range e.Values {
+				val := e.Start + i
+				if e.Multiple {
+					val = e.Start << i
+				}
 				name := e.Name + ev.Suffix
+				cmt := fmt.Sprintf("%2d %s", val, ev.Label)
 				if i == 0 {
-					g.Id(name).Id(e.Name).Op("=").Lit(e.Start).Op("+").Id("iota").Comment(ev.Label)
+					g.Id(name).Id(e.Name).Op("=").Lit(e.Start).Op(op).Id("iota").Comment(cmt)
 				} else {
-					g.Id(name).Comment(ev.Label)
+					g.Id(name).Comment(cmt)
 				}
 			}
 		})
@@ -45,9 +71,18 @@ func (e *Enum) Code() jen.Code {
 			st.Func().Params(jen.Id("z").Op("*").Id(e.Name)).Id("Decode").Params(jen.Id("s").String()).Error()
 			st.Block(jen.Switch(jen.Id("s")).BlockFunc(func(g *jen.Group) {
 				for i, ev := range e.Values {
+					val := e.Start + i
+					if e.Multiple {
+						val = e.Start << i
+					}
 					name := e.Name + ev.Suffix
-					id := fmt.Sprint(e.Start + i)
-					g.Case(jen.Lit(id), jen.Lit(LcFirst(ev.Suffix)))
+					id := fmt.Sprint(val)
+					label := ev.getLabel(e.Shorted)
+					cases := []jen.Code{jen.Lit(id), jen.Lit(label)}
+					if ss := ev.getLabel(false); ss != label && e.Shorted {
+						cases = append(cases, jen.Lit(ss))
+					}
+					g.Case(cases...)
 					g.Op("*").Id("z").Op("=").Id(name)
 				}
 				g.Default().Return(jen.Qual("fmt", "Errorf").Call(jen.Lit("invalid "+LcFirst(e.Name)+": %q"), jen.Id("s")))
@@ -68,7 +103,7 @@ func (e *Enum) Code() jen.Code {
 				for _, ev := range e.Values {
 					name := e.Name + ev.Suffix
 					g.Case(jen.Id(name))
-					g.Return(jen.Lit(LcFirst(ev.Suffix)))
+					g.Return(jen.Lit(ev.getLabel(e.Shorted)))
 				}
 				g.Default().Return(jen.Qual("fmt", "Sprintf").Call(jen.Lit(LcFirst(e.Name)+" %d"), jen.Id(e.Type).Call(jen.Id("z"))))
 			}))
