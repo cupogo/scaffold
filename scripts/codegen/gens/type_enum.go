5c4f574f3483a0775162777e9916b48a79d9f78a
liut <liutao@hyyl.xyz>
2023-10-07 14:34:39 +0800

    Add return a value as string ValStr() string for enum defines.

--- scripts/codegen/gens/type_enum.go
+++ scripts/codegen/gens/type_enum.go
@@ -25,6 +25,7 @@ type Enum struct {
 	TextUnmarshaler bool `yaml:"textUnmarshaler,omitempty"`
 	Multiple        bool `yaml:"multiple,omitempty"`
 	Shorted         bool `yaml:"shorted,omitempty"`
+	ValStr          bool `yaml:"valstr,omitempty"` // return a value as string
 }
 
 type EnumVal struct {
@@ -51,108 +52,116 @@ func (e *Enum) Code() jen.Code {
 	st := jen.Comment(e.Comment).Line()
 	st.Type().Id(e.Name).Id(e.Type).Line()
 
-	if len(e.Values) > 1 {
-		zeroStart := e.Start < 1
-		var zeroValue *EnumVal
-		vals := e.Values
-		if e.Multiple && zeroStart {
-			e.Start = 1
-			zeroValue = &vals[0]
-			vals = vals[1:]
+	if len(e.Values) <= 1 { // At least 2 vales are required
+		return st
+	}
 
+	zeroStart := e.Start < 1
+	var zeroValue *EnumVal
+	vals := e.Values
+	if e.Multiple && zeroStart {
+		e.Start = 1
+		zeroValue = &vals[0]
+		vals = vals[1:]
+
+	}
+	st.Const().DefsFunc(func(g *jen.Group) {
+		op := "+"
+		if e.Multiple {
+			op = "<<"
 		}
-		st.Const().DefsFunc(func(g *jen.Group) {
-			op := "+"
+		for i, ev := range vals {
+			val := e.Start + i
 			if e.Multiple {
-				op = "<<"
+				val = e.Start << i
+			}
+			name := e.Name + ev.Suffix
+			cmt := fmt.Sprintf("%2d %s", val, ev.Label)
+			if i == 0 {
+				g.Id(name).Id(e.Name).Op("=").Lit(e.Start).Op(op).Id("iota").Comment(cmt)
+			} else if ev.Value > 0 && i == len(vals)-1 {
+				g.Id(name).Id(e.Name).Op("=").Lit(ev.Value).Comment(cmt)
+			} else {
+				g.Id(name).Comment(cmt)
+			}
+		}
+
+		if zeroValue != nil {
+			g.Line()
+			g.Id(e.Name + zeroValue.Suffix).Id(e.Name).Op("=0").Comment(zeroValue.Label)
+		}
+	})
+
+	if e.Decodable || e.TextUnmarshaler {
+		st.Line()
+		st.Func().Params(jen.Id("z").Op("*").Id(e.Name)).Id("Decode").Params(jen.Id("s").String()).Error()
+		st.Block(jen.Switch(jen.Id("s")).BlockFunc(func(g *jen.Group) {
+			if zeroValue != nil {
+				code := zeroValue.getCode(e.Shorted)
+				cases := []jen.Code{jen.Lit("0"), jen.Lit(code)}
+				if ss := zeroValue.getCode(false); ss != code && e.Shorted {
+					cases = append(cases, jen.Lit(ss))
+				}
+				g.Case(cases...)
+				g.Op("*").Id("z").Op("=").Id(e.Name + zeroValue.Suffix)
 			}
 			for i, ev := range vals {
 				val := e.Start + i
 				if e.Multiple {
 					val = e.Start << i
+				} else if ev.Value > 0 && i == len(vals)-1 {
+					val = ev.Value
 				}
 				name := e.Name + ev.Suffix
-				cmt := fmt.Sprintf("%2d %s", val, ev.Label)
-				if i == 0 {
-					g.Id(name).Id(e.Name).Op("=").Lit(e.Start).Op(op).Id("iota").Comment(cmt)
-				} else if ev.Value > 0 && i == len(vals)-1 {
-					g.Id(name).Id(e.Name).Op("=").Lit(ev.Value).Comment(cmt)
-				} else {
-					g.Id(name).Comment(cmt)
+				id := fmt.Sprint(val)
+				code := ev.getCode(e.Shorted)
+				cases := []jen.Code{jen.Lit(id), jen.Lit(code)}
+				if ss := ev.getCode(false); ss != code && e.Shorted {
+					cases = append(cases, jen.Lit(ss))
 				}
+				if code != ev.Suffix {
+					cases = append(cases, jen.Lit(ev.Suffix))
+				}
+				if IsAlphaOnly(ev.Label) && code != ev.Label && ev.Suffix != ev.Label {
+					cases = append(cases, jen.Lit(ev.Label))
+				}
+				g.Case(cases...)
+				g.Op("*").Id("z").Op("=").Id(name)
 			}
+			g.Default().Return(jen.Qual("fmt", "Errorf").Call(jen.Lit("invalid "+LcFirst(e.Name)+": %q"), jen.Id("s")))
 
-			if zeroValue != nil {
-				g.Line()
-				g.Id(e.Name + zeroValue.Suffix).Id(e.Name).Op("=0").Comment(zeroValue.Label)
-			}
-		})
+		}), jen.Return(jen.Nil()))
 
-		if e.Decodable || e.TextUnmarshaler {
+		if e.TextUnmarshaler {
 			st.Line()
-			st.Func().Params(jen.Id("z").Op("*").Id(e.Name)).Id("Decode").Params(jen.Id("s").String()).Error()
-			st.Block(jen.Switch(jen.Id("s")).BlockFunc(func(g *jen.Group) {
-				if zeroValue != nil {
-					code := zeroValue.getCode(e.Shorted)
-					cases := []jen.Code{jen.Lit("0"), jen.Lit(code)}
-					if ss := zeroValue.getCode(false); ss != code && e.Shorted {
-						cases = append(cases, jen.Lit(ss))
-					}
-					g.Case(cases...)
-					g.Op("*").Id("z").Op("=").Id(e.Name + zeroValue.Suffix)
-				}
-				for i, ev := range vals {
-					val := e.Start + i
-					if e.Multiple {
-						val = e.Start << i
-					} else if ev.Value > 0 && i == len(vals)-1 {
-						val = ev.Value
-					}
-					name := e.Name + ev.Suffix
-					id := fmt.Sprint(val)
-					code := ev.getCode(e.Shorted)
-					cases := []jen.Code{jen.Lit(id), jen.Lit(code)}
-					if ss := ev.getCode(false); ss != code && e.Shorted {
-						cases = append(cases, jen.Lit(ss))
-					}
-					if code != ev.Suffix {
-						cases = append(cases, jen.Lit(ev.Suffix))
-					}
-					if IsAlphaOnly(ev.Label) && code != ev.Label && ev.Suffix != ev.Label {
-						cases = append(cases, jen.Lit(ev.Label))
-					}
-					g.Case(cases...)
-					g.Op("*").Id("z").Op("=").Id(name)
-				}
-				g.Default().Return(jen.Qual("fmt", "Errorf").Call(jen.Lit("invalid "+LcFirst(e.Name)+": %q"), jen.Id("s")))
-
-			}), jen.Return(jen.Nil()))
-
-			if e.TextUnmarshaler {
-				st.Line()
-				st.Func().Params(jen.Id("z").Op("*").Id(e.Name)).Id("UnmarshalText").Params(jen.Id("b").Index().Byte()).Error()
-				st.Block(jen.Return(jen.Id("z").Dot("Decode").Call(jen.String().Call(jen.Id("b")))))
-			}
+			st.Func().Params(jen.Id("z").Op("*").Id(e.Name)).Id("UnmarshalText").Params(jen.Id("b").Index().Byte()).Error()
+			st.Block(jen.Return(jen.Id("z").Dot("Decode").Call(jen.String().Call(jen.Id("b")))))
 		}
+	}
 
-		if e.Stringer || e.TextMarshaler {
+	if e.Stringer || e.TextMarshaler {
+		st.Line()
+		st.Func().Params(jen.Id("z").Id(e.Name)).Id("String").Params().String()
+		st.Block(jen.Switch(jen.Id("z")).BlockFunc(func(g *jen.Group) {
+			for _, ev := range e.Values {
+				name := e.Name + ev.Suffix
+				g.Case(jen.Id(name))
+				g.Return(jen.Lit(ev.getCode(e.Shorted)))
+			}
+			g.Default().Return(jen.Qual("fmt", "Sprintf").Call(jen.Lit(LcFirst(e.Name)+" %d"), jen.Id(e.Type).Call(jen.Id("z"))))
+		}))
+
+		if e.TextMarshaler {
 			st.Line()
-			st.Func().Params(jen.Id("z").Id(e.Name)).Id("String").Params().String()
-			st.Block(jen.Switch(jen.Id("z")).BlockFunc(func(g *jen.Group) {
-				for _, ev := range e.Values {
-					name := e.Name + ev.Suffix
-					g.Case(jen.Id(name))
-					g.Return(jen.Lit(ev.getCode(e.Shorted)))
-				}
-				g.Default().Return(jen.Qual("fmt", "Sprintf").Call(jen.Lit(LcFirst(e.Name)+" %d"), jen.Id(e.Type).Call(jen.Id("z"))))
-			}))
-
-			if e.TextMarshaler {
-				st.Line()
-				st.Func().Params(jen.Id("z").Id(e.Name)).Id("MarshalText").Params().Params(jen.Index().Byte(), jen.Error())
-				st.Block(jen.Return(jen.Index().Byte().Params(jen.Id("z").Dot("String").Call()), jen.Nil()))
-			}
+			st.Func().Params(jen.Id("z").Id(e.Name)).Id("MarshalText").Params().Params(jen.Index().Byte(), jen.Error())
+			st.Block(jen.Return(jen.Index().Byte().Params(jen.Id("z").Dot("String").Call()), jen.Nil()))
 		}
+	}
+
+	if e.ValStr {
+		st.Line()
+		st.Func().Params(jen.Id("z").Id(e.Name)).Id("ValStr").Params().String()
+		st.Block(jen.Return().Qual("strconv", "Itoa").Call(jen.Int().Call(jen.Id("z"))))
 	}
 
 	return st
